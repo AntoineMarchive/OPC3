d81f8d9abc3449844001b0560ffbbdbfa0d84e93
Antoine Marchive <antoine.marchive@orange.fr>
2025-02-13 14:07:14 +0100

    2nd commit : modification accountOpportunityViewer.js

--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -4,7 +4,7 @@ import { refreshApex } from '@salesforce/apex';
 
 export default class AccountOpportunitiesViewer extends LightningElement {
     @api recordId;
-    @track opportunities;
+    @track opportunities = []; // Liste des opportunités récupérées
     @track error = {};
     @track showError = false; // ajout d'une propriété pour afficher ou non l'erreur
     wiredOpportunities // ajout d'une propriété pour stocker le résultat de l'appel Apex
@@ -16,12 +16,15 @@ export default class AccountOpportunitiesViewer extends LightningElement {
         { label: 'Phase', fieldName: 'StageName', type: 'text' }
     ];
 
+    // Colonnes définies pour le tableau des opportunités
     @wire(getOpportunities, { accountId: '$recordId' }) //error inversion recordId avec accountId
     wiredOpportunities({ error, data }) {
         if (data) {
+            // Récupération des opportunités via un appel Apex
             this.opportunities = data;
             this.showError = false; // Réinitialiser l'affichage de l'erreur
         } else if (error) {
+            // En cas d'erreur, stockez-la pour un affichage
             this.error = error;
             this.opportunities = undefined;
             this.showError = true; // Activer l'affichage de l'erreur
